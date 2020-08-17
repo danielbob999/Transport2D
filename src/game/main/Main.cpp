a193c028db795a974d1b5a7264b0ccd37a10923f
danielbob999 <danielbob999@users.noreply.github.com>
2020-08-17 14:10:53 +1200

    Added the Box2d library and includes

--- src/game/main/Main.cpp
+++ src/game/main/Main.cpp
@@ -2,10 +2,20 @@
 #include "../../core/core.h"
 #include "../../core/console/Console.h"
 
+#include "../../../includes/box2d/box2d.h"
+
+b2World world(b2Vec2(0.0f, 0.0f));
+
 void start() {
+	b2Vec2 gravity(0.0f, -10.0f);
+	world = b2World(gravity);
 }
 
 void update(double delta) {
+	float timeStep = 1.0f / 60.0f;
+	world.Step(timeStep, 6, 2);
+
+	Console::log(std::to_string(world.GetGravity().x) + ", " + std::to_string(world.GetGravity().y));
 }
 
 void close() {
