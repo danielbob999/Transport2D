d7aace87c5203c1e28c68502cb5135c565fc179a
danielbob999 <danielrobinson999@gmail.com>
2020-09-23 16:00:29 +1200

    Added some extra spicy functions to help with parent object tracking

--- src/core/objectsystem/ComponentScript.cpp
+++ src/core/objectsystem/ComponentScript.cpp
@@ -1,3 +1,11 @@
 #include "ComponentScript.h"
 using namespace core_objectsystem;
 
+int ComponentScript::getParentId() {
+	return m_parentId;
+}
+
+void ComponentScript::setParentId(int id) {
+	m_parentId = id;
+}
+
