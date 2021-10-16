2b2de62fc918488edbf5678eb0c9e56c83d15dfe
danielbob999 <danielrobinson999@gmail.com>
2021-10-16 14:15:45 +1300

    Added SDL2 files

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -5,6 +5,7 @@
 #include "input/InputSystem.h"
 #include "renderer/RenderSystem.h"
 #include "renderer/Camera.h"
+#include "SDL/SDL.h"
 
 using namespace core_input;
 using namespace core_objectsystem;
@@ -63,6 +64,28 @@ void Core::run() {
 
     ImGui_ImplGlfw_InitForOpenGL(m_window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
+
+    // start sdl
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+        //Create window
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+    }
+
+    // end sdl
 
     /* 
      ===================================
